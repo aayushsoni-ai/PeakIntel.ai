3f7872e55815c30a7d0d00b9f222fc2bd6be139e
Aayush Soni <aayushsoni516@gmail.com>
2026-08-29 11:39:58 +0530

    fix Next.js strict trailing slash bug for socket.io rewrites

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
         destination: "http://peakin-publi-wyhvz0nrfxzp-317908020.ap-south-1.elb.amazonaws.com/:path*",
       },
       {
+        source: "/socket.io",
+        destination: "http://peakin-publi-wyhvz0nrfxzp-317908020.ap-south-1.elb.amazonaws.com/socket.io",
+      },
+      {
         source: "/socket.io/:path*",
         destination: "http://peakin-publi-wyhvz0nrfxzp-317908020.ap-south-1.elb.amazonaws.com/socket.io/:path*",
       },
